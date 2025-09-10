bba8e506748abc387f3a6e294e92a4b355f5424e
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-09-10 23:38:12 +0100

    Refactor IndicesCard to use Image component and update index data structure to include icon URLs

--- src/components/indices/IndicesCard.tsx
+++ src/components/indices/IndicesCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Index } from "../../data/indices";
 import PercentageChange from "../common/PercentChange";
+import Image from "next/image";
 
 interface IndicesCardProps {
   index: Index;
@@ -10,12 +11,14 @@ const IndicesCard = ({ index }: IndicesCardProps) => {
   return (
     <div className="flex flex-col items-center text-center gap-2 rounded-2xl font-family-open-sauce py-3 px-[17px] h-[156px] border border-[#E8EBED] hover:border-gray-200 transition-colors">
       <div className="flex flex-col items-center gap-2">
-        <div
-          className={`w-8 h-8 ${index.iconColor} rounded-full flex items-center justify-center text-white font-bold text-lg`}
-        >
-          {index.iconText}
-        </div>
-        
+        <Image
+          className={`w-8 h-8 rounded-full `}
+          src={index.icon}
+          alt={index.symbol}
+          width={32}
+          height={32}
+        />
+
         <div>
           <h3 className="text-sm font-bold text-[#090A0B] mb-1">
             {index.symbol}
--- src/data/indices.ts
+++ src/data/indices.ts
@@ -5,8 +5,7 @@ export type Index = {
     price: number;
     changePercent: number;
     direction: 'up' | 'down';
-    iconColor: string;
-    iconText: string;
+    icon: string;
 };
 
 export const indices: Index[] = [
@@ -17,8 +16,7 @@ export const indices: Index[] = [
         price: 4365.99,
         changePercent: 0.10,
         direction: 'up',
-        iconColor: 'bg-red-600',
-        iconText: '500'
+        icon: 'https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537764/genmoney/Ellipse_10_ywq2rw.png'
     },
     {
         id: 'ndq',
@@ -27,8 +25,7 @@ export const indices: Index[] = [
         price: 15154.93,
         changePercent: -0.10,
         direction: 'down',
-        iconColor: 'bg-blue-500',
-        iconText: '100'
+        icon: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537806/genmoney/image_ydhe7m.png"
     },
     {
         id: 'spx2',
@@ -37,8 +34,7 @@ export const indices: Index[] = [
         price: 4365.99,
         changePercent: 0.01,
         direction: 'up',
-        iconColor: 'bg-red-600',
-        iconText: '500'
+        icon: 'https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537764/genmoney/Ellipse_10_ywq2rw.png'
     },
     {
         id: 'vix1',
@@ -47,8 +43,7 @@ export const indices: Index[] = [
         price: 14.89,
         changePercent: -0.01,
         direction: 'down',
-        iconColor: 'bg-green-600',
-        iconText: '>'
+        icon: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537761/genmoney/image-1_fwwfud.png"
     },
     {
         id: 'dxy',
@@ -57,8 +52,7 @@ export const indices: Index[] = [
         price: 105.341,
         changePercent: 0.11,
         direction: 'up',
-        iconColor: 'bg-teal-600',
-        iconText: '$'
+        icon: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537763/genmoney/image-2_y8oid4.png"
     },
     {
         id: 'vix2',
@@ -67,7 +61,6 @@ export const indices: Index[] = [
         price: 14.89,
         changePercent: -0.01,
         direction: 'down',
-        iconColor: 'bg-green-600',
-        iconText: '>'
+        icon: "https://res.cloudinary.com/dhvwthnzq/image/upload/v1757537761/genmoney/image-1_fwwfud.png"
     }
 ];
